ae3a78c28a5d7a5c3ad4670f5003209592efd6a4
Mursal Nasaj <nasajmursal@gmail.com>
2023-05-07 07:48:29 -0700

    finish

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(window).on("load",function(){
+  $(".preloader").addClass("loaded")
+})
+
+
+$(document).ready(function () {
+
+    // nav toggle
+    $(".nav-toggle").click(function () {
+        $(".header .nav").slideToggle()
+    })
+    $("header .nav a").click(function () { 
+        if($(window).width() < 768){
+            $(".header .nav").slideToggle()
+        }
+        
+    });
+
+    // fixed header
+    $(window).scroll(function (){
+        if($(this).scrollTop() > 100){
+            $("header").addClass("fixed")
+        }else {
+            $("header").removeClass("fixed")
+        }
+    })
+
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+});
+
+
+$(function () {
+    $(".work-section").magnificPopup({
+      delegate: "a",
+      type: "image",
+      gallery: {
+        enabled: true,
+      },
+    });
+  });
